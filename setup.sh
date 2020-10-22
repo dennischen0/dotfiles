37901bc47a31a5de28b549ce0e48ff2940a69a72
Dennis Chen <c.w.dennis@gmail.com>
2020-10-22 01:19:56 -0700

    install zsh and omz

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+    brew install zsh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Installing zsh"
+    apt install zsh
+    zsh --version
+    chsh -s $(which zsh)
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+fi
 # Copy theme to directory
-cp ./dennis.zsh-theme ~/.oh-my-zsh/themes/dennis.zsh-theme
+cp ./dennis.zsh-theme /home/dennischen/.oh-my-zsh/themes/dennis.zsh-theme
